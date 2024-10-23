e66e557e43f4b23ce76c77dc8f9ac2424db663d1
p3rcypj <pablo@percypj.com>
2024-10-23 19:26:59 +0000

    Dont hide empty columns

--- app/search/search.controllers.js
+++ app/search/search.controllers.js
@@ -327,7 +327,7 @@ searchModule.controller("searchController", [
                 },
                 publicAccess: "--------",
                 type: "PIVOT_TABLE",
-                hideEmptyColumns: true,
+                hideEmptyColumns: false,
                 hideEmptyRows: false,
                 subscribed: false,
                 parentGraphMap: {},
