7b769fe9cfa2ab62b79a0a1f5d407a88a37ad4de
p3rcypj <pablo@percypj.com>
2024-11-22 12:25:20 +0000

    Add help icons on dossierIndicators page

--- app/helpIcon/helpIcon.directive.js
+++ app/helpIcon/helpIcon.directive.js
@@ -3,7 +3,13 @@ helpIconModule.directive("helpIcon", function () {
         restrict: "E",
         scope: {
             title: "@",
+            size: "@",
         },
         templateUrl: "app/helpIcon/helpIcon.template.html",
+        link: function (_scope, _element, attrs) {
+            if (attrs.size !== "small" && attrs.size !== "medium") {
+                attrs.size = "small"; // default to medium if size is not valid
+            }
+        },
     };
 });
